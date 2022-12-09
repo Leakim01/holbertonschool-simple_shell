3b36d64016fc3d42d0dafeec769d3502059ccd3c
RedginaldGodeau <5657@holbertonstudents.com>
2022-12-09 05:00:57 -0800

    correcting some valgrind problem

--- exec_cmd.c
+++ exec_cmd.c
@@ -8,34 +8,41 @@
  */
 int exec_cmd(char *av[], char *ev[])
 {
-	const char *path = "/bin/";
-	char *cmd;
+	char *path =  "/bin/";
+	char tmp[1024];
 	pid_t pid;
 	int error = 0;
 	int have_path = !strstr(av[0], "./") && !strstr(av[0], "/bin/");
 
-	if (!av[0])
+	if (!av[0])		
 		return (-3);
+
 	if (have_path)
 	{
-			cmd = strdup(path);
-			strcat(cmd, av[0]);
-			av[0] = strdup(cmd);
-			free(cmd);
+		strcpy(tmp, path);
+		strcat(tmp, av[0]);
+		av[0] = strdup(tmp);
 	}
 
 	pid = fork();
 	switch (pid)
 	{
 		case -1:
+			pid = wait(NULL);
+			if (have_path)
+				free(av[0]);
 			error = 4;
 			break;
 		case 0:
 			if (execve(av[0], av, ev) == -1)
 				printf("EXECVE ERROR\n");
+			if (have_path)
+				free(av[0]);
 			exit(1);
 			break;
 		case 1:
+			if (have_path)
+				free(av[0]);
 			error = 0;
 			break;
 	}
